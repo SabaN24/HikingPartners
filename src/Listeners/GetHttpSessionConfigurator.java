5dde169d5b22e5bf4907d5a08c01eac8dd66b541
levani132 <lberoshvili9@gmail.com>
2017-06-19 20:48:14 +0400

    Added http session configurator

---
+++ src/Listeners/GetHttpSessionConfigurator.java
@@ -0,0 +1,22 @@
+package Listeners;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+/**
+ * Created by Levani on 19.06.2017.
+ */
+
+public class GetHttpSessionConfigurator extends ServerEndpointConfig.Configurator
+{
+    @Override
+    public void modifyHandshake(ServerEndpointConfig config,
+                                HandshakeRequest request,
+                                HandshakeResponse response)
+    {
+        HttpSession httpSession = (HttpSession)request.getHttpSession();
+        config.getUserProperties().put(HttpSession.class.getName(),httpSession);
+    }
+}
